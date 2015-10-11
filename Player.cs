e0bfbda556777e642861110b9eb13a7cd2c34f7d
David Long <longda@gmail.com>
2015-10-11 16:17:49 -0700

    lesson 2 - gun system

--- Player.cs
+++ Player.cs
@@ -2,25 +2,30 @@
 using System.Collections;
 
 [RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(GunController))]
 public class Player : MonoBehaviour 
 {
 	public float moveSpeed = 5;
 	
 	Camera viewCamera;
 	PlayerController controller;
+	GunController gunController;
 
 	void Start () 
 	{
 		controller = GetComponent<PlayerController>();
+		gunController = GetComponent<GunController>();
 		viewCamera = Camera.main;
 	}
 	
 	void Update () 
 	{
+		// Movement input
 		var moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 		var moveVelocity = moveInput.normalized * moveSpeed;
 		controller.Move(moveVelocity);
 		
+		// Look input
 		var ray = viewCamera.ScreenPointToRay(Input.mousePosition);
 		var groundPlane = new Plane(Vector3.up, Vector3.zero);
 		float rayDistance;
@@ -28,8 +33,14 @@ public class Player : MonoBehaviour
 		if (groundPlane.Raycast(ray, out rayDistance))
 		{
 			var point = ray.GetPoint(rayDistance);
-			Debug.DrawLine(ray.origin, point, Color.red);
+			//Debug.DrawLine(ray.origin, point, Color.red);
 			controller.LookAt(point);
+		}
+		
+		// Weapon input
+		if (Input.GetMouseButton(0))
+		{
+			gunController.Shoot();
 		}
 	}
 }
