27b98f6fba99736a2ac2ae10bf8d004fe9a422a7
David Long <longda@gmail.com>
2015-10-24 15:35:40 -0700

    obstacle placement

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour 
 {
 	public Transform tilePrefab;
+	public Transform obstaclePrefab;
 	public Vector2 mapSize;
 	
 	[Range(0,1)]
 	public float outlinePercent;
+	
+	List<Coord> allTileCoords;
+	Queue<Coord> shuffledTileCoords;
+	
+	public int seed = 10;
 	
 	public void Start()
 	{
@@ -16,6 +23,16 @@ public class MapGenerator : MonoBehaviour
 	
 	public void GenerateMap()
 	{
+		allTileCoords = new List<Coord>();
+		for (var x = 0; x < mapSize.x; x++)
+		{
+			for (var y = 0; y < mapSize.y; y++)
+			{
+				allTileCoords.Add(new Coord(x, y));
+			}
+		}
+		shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+		
 		var holderName = "Generated Map";
 		if (transform.FindChild(holderName)) 
 		{
@@ -34,6 +51,40 @@ public class MapGenerator : MonoBehaviour
 				newTile.localScale = Vector3.one * (1 - outlinePercent);
 				newTile.parent = mapHolder;
 			}
+		}
+		
+		var obstacleCount = 10;
+		for (var i = 0; i < obstacleCount; i++)
+		{
+			var randomCoord = GetRandomCoord();
+			var obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+			var newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+			newObstacle.parent = mapHolder;
+		}
+	}
+	
+	Vector3 CoordToPosition(int x, int y)
+	{
+		return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+	}
+	
+	public Coord GetRandomCoord()
+	{
+		var randomCoord = shuffledTileCoords.Dequeue();
+		shuffledTileCoords.Enqueue(randomCoord);
+		
+		return randomCoord;
+	}
+	
+	public struct Coord
+	{
+		public int x;
+		public int y;
+		
+		public Coord(int _x, int _y)
+		{
+			x = _x;
+			y = _y;
 		}
 	}
 }
---
+++ Utility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+public static class Utility
+{
+	public static T[] ShuffleArray<T>(T[] array, int seed)
+	{
+		var prng = new System.Random(seed);
+		
+		for (var i = 0; i < array.Length - 1; i++)
+		{
+			var randomIndex = prng.Next(i, array.Length);
+			var tempItem = array[randomIndex];
+			array[randomIndex] = array[i];
+			array[i] = tempItem;
+		}
+		
+		return array;
+	}
+}
