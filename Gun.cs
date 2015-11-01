26f5af5c5a41a73fe8c62511c07e0de2b2603672
David Long <longda@gmail.com>
2015-10-31 21:25:52 -0700

    lesson 16 - weapon effects

--- Gun.cs
+++ Gun.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(MuzzleFlash))]
 public class Gun : MonoBehaviour 
 {
 	public Transform muzzle;
 	public Projectile projectile;
 	public float msBetweenShots = 100;
 	public float muzzleVelocity = 35;
+	
+	public Transform shell;
+	public Transform shellEjector;
+	MuzzleFlash muzzleFlash;
+	
+	void Start()
+	{
+		muzzleFlash = GetComponent<MuzzleFlash>();
+	}
 	
 	float nextShotTime;
 	
@@ -17,6 +27,9 @@ public class Gun : MonoBehaviour
 			nextShotTime = Time.time + msBetweenShots / 1000;
 			var newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
 			newProjectile.SetSpeed(muzzleVelocity);
+			
+			Instantiate(shell, shellEjector.position, shellEjector.rotation);
+			muzzleFlash.Activate();
 		}	
 	}
 }
---
+++ Shell.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour 
+{
+	public Rigidbody myRigidBody;
+	public float forceMin;
+	public float forceMax;
+	
+	float lifetime = 4f;
+	float fadetime = 2f;
+	
+	void Start () 
+	{
+		var force = Random.Range(forceMin, forceMax);
+		myRigidBody.AddForce(transform.right * force);
+		myRigidBody.AddTorque(Random.insideUnitSphere * force);
+		
+		StartCoroutine(Fade());
+	}
+	
+	IEnumerator Fade()
+	{
+		yield return new WaitForSeconds(lifetime);
+		
+		var percent = 0f;
+		var fadeSpeed = 1 / fadetime;
+		var mat = GetComponent<Renderer>().material;
+		var initialColor = mat.color;
+		
+		while (percent < 1)
+		{
+			percent += Time.deltaTime * fadeSpeed;
+			mat.color = Color.Lerp(initialColor, Color.clear, percent);
+			yield return null;
+		}
+		
+		Destroy(gameObject);
+	}
+}
